4aeadb3590707c65180388a2e67f02e606865dca
Van Vien Dong <dong.le@sotatek.com>
2023-02-26 17:04:58 +0700

    clean up

--- src/pages/InvalidPage.tsx
+++ src/pages/InvalidPage.tsx
@@ -5,13 +5,10 @@ import styles from "./InvalidPage.module.scss";
 const InvalidPage: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className={`${styles.container}`}>
-      <p className={`${styles.title}`}>404</p>
-      <p className={`${styles.message}`}>The page could not be found !</p>
-      <button
-        className="primary-button"
-        onClick={() => navigate(PATH.NOW_PLAYING)}
-      >
+    <div className={styles.container}>
+      <p className={styles.title}>404</p>
+      <p className={styles.message}>The page could not be found !</p>
+      <button onClick={() => navigate(PATH.NOW_PLAYING)}>
         Go To Home Page
       </button>
     </div>
